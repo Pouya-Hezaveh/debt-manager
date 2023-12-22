1c49d09751a39a8fb8dcb9cc5966176e0899ec1e
Hezavehir <hezavehir@gmail.com>
2023-12-23 02:08:17 +0330

    Creating the client app in React framework. using command: npx create-react-app client

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 // server/index.js
 
 const express = require("express");
+const { env } = require("process");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>THIS IS THE MAIN WEBPAGE.</h1>");
+  console.log(" * new root request!");
+});
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+  console.log(" * new API request!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
