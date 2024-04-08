94aa32bd521601d7b186759044c1bba237a1dce9
Hezavehir <hezavehir@gmail.com>
2024-04-08 17:19:27 +0330

    Revert "wrap up!"
    This reverts commit f62ecf9ff2ad1aeafdf700266fb5634197b1e4b6.

--- start-dev-server.sh
+++ start-dev-server.sh
@@ -1,1 +1,2 @@
-lsof -i :3000 -i :3001 | awk '{print $2}' | xargs kill -9; npm run --prefix . start & npm run --prefix ./client start
+sudo lsof -i :3000 -i :3001 | awk '{print $2}' | xargs kill -9; npm run --prefix . start & npm run --prefix ./client start
+
