c540c5a28297ac524dcfdb43daae18e964b999a9
Hezavehir <hezavehir@gmail.com>
2024-04-08 17:21:27 +0330

    Wrapping Up!

--- start-dev-server.sh
+++ start-dev-server.sh
@@ -1,2 +1,1 @@
-sudo lsof -i :3000 -i :3001 | awk '{print $2}' | xargs kill -9; npm run --prefix . start & npm run --prefix ./client start
-
+lsof -i :3000 -i :3001 | awk '{print $2}' | xargs kill -9; npm run --prefix . start & npm run --prefix ./client start
